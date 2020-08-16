5c5438fac96897ac76ceabae865bd4aaf3ba12d4
Suhku Huh <nineclue@gmail.com>
2020-08-16 22:35:35 +0900

    basic layout & tableview 추가

--- src/main/scala/DataStage.scala
+++ src/main/scala/DataStage.scala
@@ -1,23 +1,43 @@
 import javafx.stage.Stage
 import javafx.stage.StageStyle
 import javafx.stage.Modality
+import javafx.scene.Node
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
 import javafx.scene.control.DatePicker
-import javafx.scene.control.ListView
-import java.{util => ju}
+import javafx.scene.control.{TableView, TableColumn}
+
 import java.time.LocalDate
-import javafx.scene.layout.Pane
+import javafx.scene.layout.{Pane, GridPane, ColumnConstraints, RowConstraints, Priority}
 import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
 import javafx.scene.Scene
 import javafx.scene.control.Button
-import javafx.geometry.Insets
+import javafx.geometry.{Insets, HPos}
 import java.awt.Event
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
 
-case class PatientRecord(iday: LocalDate, height: Double, weight: Double)
+// import javafx.beans.property.{SimpleListProperty
+import javafx.beans.property.{SimpleStringProperty, SimpleDoubleProperty}
+import javafx.beans.value.{ChangeListener, ObservableValue}
+import javafx.collections.{FXCollections, ListChangeListener}
+import javafx.scene.control.cell.PropertyValueFactory
+
+case class PatientRecord(iday: LocalDate, height: Double, weight: Double) {
+    private val idayProperty = new SimpleStringProperty(iday.toString)
+    private val heightProperty = new SimpleDoubleProperty(height)
+    private val weightProperty = new SimpleDoubleProperty(weight)
+
+    def getIday() = idayProperty.get
+    def setIday(d: String) = LocalDate.parse(d)
+
+    def getHeight = heightProperty.get
+    def setHeight(h: Double) = heightProperty.set(h)
+
+    def getWeight = weightProperty.get
+    def setWeight(w: Double) = weightProperty.set(w)
+}
 
 trait PatientInput {
     val chartLabel: Label 
@@ -37,11 +57,13 @@ trait PatientInput {
 
     val commitButton: Button
 
-    val records: ListView[PatientRecord]
+    val records: TableView[PatientRecord]
 }
 
 object DataStage {
     type InputPane = (Pane, PatientInput)
+
+    /*
     def apply(parent: Stage): Stage = {
         val ds = new Stage(StageStyle.UNDECORATED)
         ds.initOwner(parent)
@@ -53,9 +75,10 @@ object DataStage {
         ds.setScene(new Scene(box))
         ds
     }
+    */
 
-    private def inputPane(): InputPane = {
-        val pi = new PatientInput {
+    def inputPane(): InputPane = {
+        val pi: PatientInput = new PatientInput {
             val chartLabel: Label = new Label("차트번호")
             val chartInput: TextField = new TextField()
 
@@ -72,21 +95,96 @@ object DataStage {
             val bmiValue: Label = new Label("0")
 
             val commitButton: Button = new Button("저장 및 그래프")
-            val records: ListView[PatientRecord] = new ListView[PatientRecord]()
+            val records: TableView[PatientRecord] = new TableView[PatientRecord]()
         }
 
-        val b1 = new HBox(pi.chartLabel, pi.chartInput)
-        val b2 = new HBox(pi.bdayLabel, pi.bdayInput)
-        val b3 = new HBox(pi.idayLabel, pi.idayInput)
-        val b4 = new HBox(pi.heightLabel, pi.heigthInput)
-        val b5 = new HBox(pi.weightLabel, pi.weightInput)
-        val b6 = new HBox(pi.bmiLabel, pi.bmiValue)
-        val b7 = new HBox(pi.commitButton)
- 
-        Seq(b1,b2,b3,b4,b5,b6,b7).foreach(setPadSpace)
-        (new VBox(b1, b2, b3, b4, b5, b6, b7, pi.records), pi)
+        val p = new GridPane()
+        val firstCol: Seq[Node] = Seq(pi.chartLabel, pi.bdayLabel, pi.idayLabel, pi.heightLabel, pi.weightLabel, pi.bmiLabel)
+        firstCol.zipWithIndex.foreach({ case (n, i) =>
+            setGridPos(n, 0, i)
+        })
+        val secondCol: Seq[Node] = Seq(pi.chartInput, pi.bdayInput, pi.idayInput, pi.heigthInput, pi.weightInput, pi.bmiValue, pi.commitButton)
+        secondCol.zipWithIndex.foreach({ case (n, i) =>
+            setGridPos(n, 1, i)
+        })
+        setGridPos(pi.records, 0, 7)
+        GridPane.setColumnSpan(pi.records, 2)
+        val children = firstCol ++ secondCol :+ pi.records
+        children.foreach(c => GridPane.setMargin(c, new Insets(3)))
+        firstCol.foreach(c => GridPane.setHalignment(c, HPos.RIGHT))
+        p.getChildren.addAll(children:_*)
+
+        val c1con = new ColumnConstraints()
+        c1con.setPercentWidth(30)
+        val c2con = new ColumnConstraints()
+        c2con.setPercentWidth(70)
+        p.getColumnConstraints.addAll(c1con, c2con)
+
+        val listRow = new RowConstraints()
+        listRow.setMaxHeight(Double.MaxValue)
+        listRow.setVgrow(Priority.ALWAYS)
+        p.getRowConstraints.addAll((Range(0,7).map(_ => new RowConstraints()) :+ listRow):_*)
+
+        p.setPadding(new Insets(10))
+        val rs = tableSet[PatientRecord](pi.records)
+        rs.add(PatientRecord(LocalDate.now, 182.3, 82.0))
+        // p.setInsets(10)
+        (p, pi)
     }
 
+    private def tableSet[A](t: TableView[A]) = {
+        t.setEditable(false)
+        
+        val c1 = new TableColumn[A, String]("기록일")
+        val c2 = new TableColumn[A, Double]("키")
+        val c3 = new TableColumn[A, Double]("몸무게")
+        t.getColumns.addAll(c1, c2, c3)
+
+        Seq(c1, c2, c3).foreach({ c => 
+            c.setResizable(false)
+            c.setSortable(false)
+            c.setReorderable(false)
+        })
+        t.widthProperty.addListener(new ChangeListener[Number] {
+            def changed(ob: ObservableValue[_ <: Number], ov: Number, nv: Number) = {
+                val tw = nv.doubleValue
+                val c1pw = tw * 0.4
+                val c2pw = tw * 0.3
+                val c3pw = tw - (c1pw + c2pw) - 2  // table grows in mac without extraspace
+                c1.setPrefWidth(c1pw)
+                c2.setPrefWidth(c2pw)
+                c3.setPrefWidth(c3pw)
+            }
+        })
+
+        /* // auto growing table
+        c1.prefWidthProperty.bind(t.widthProperty().multiply(0.4))
+        c2.prefWidthProperty.bind(t.widthProperty().multiply(0.3))
+        c3.prefWidthProperty.bind(t.widthProperty().multiply(0.3))
+        */
+
+        c1.setCellValueFactory(new PropertyValueFactory[A, String]("iday"))
+        c2.setCellValueFactory(new PropertyValueFactory[A, Double]("height"))
+        c3.setCellValueFactory(new PropertyValueFactory[A, Double]("weight"))
+
+        val rs = FXCollections.observableArrayList[A]()
+        t.setItems(rs)
+        /*
+        t.getSelectionModel.getSelectedIndices.addListener(
+        // (obs, oldsel, newsel) => println("changed")
+            new ListChangeListener[Int] {
+            def onChanged(c: ListChangeListener.Change[_ >: Int]) = println("changed")
+            }
+        )
+        */
+        // t.setContextMenu(...)
+        rs
+    }
+
+    private def setGridPos(n: Node, x: Int, y: Int) = {
+        GridPane.setColumnIndex(n, x)
+        GridPane.setRowIndex(n, y)
+    }
     private def setPadSpace(p: HBox) = {
         p.setSpacing(10)
         p.setPadding(new Insets(3))
--- src/main/scala/GChart.scala
+++ src/main/scala/GChart.scala
@@ -21,24 +21,26 @@ class GChart_ extends Application {
     val stageHeight = 800
     val flarge = Font.loadFont(getClass.getResourceAsStream("BMEULJIROTTF.ttf"), 20)
     val fsmall = Font.loadFont(getClass.getResourceAsStream("NanumMyeongjo.ttf"), 12)
-    val chart = new Chart(stageWidth, stageHeight - 100, Some(fsmall))
+    val chart = new Chart(stageWidth, stageHeight, Some(fsmall))
 
     override def start(ps: Stage) = {
-        // val text = new TextArea()
         val root = new BorderPane(chart)
-        // root.setPrefSize(600, 600)
+        /*
         val l = new Label(s"배달의 민족 : Pi는 ${DB.test}입니다.")
         l.setFont(flarge)
         l.setMinHeight(40)
         l.setPrefHeight(40)
         l.setMaxHeight(40)
         root.setBottom(l)
-        
-        val scene = new Scene(root, stageWidth, stageHeight, Color.WHITE)
+        */
+        val (bp, cs) = DataStage.inputPane()
+        root.setLeft(bp)
+
+        val scene = new Scene(root, Color.WHITE)
         ps.setScene(scene)
         ps.setTitle("성장 관리")
-        ps.setMinWidth(stageWidth)
-        ps.setMinHeight(stageWidth)
+        // ps.setMinWidth(stageWidth)
+        // ps.setMinHeight(stageWidth)
         
         /*
         root.prefHeightProperty().bind(scene.heightProperty());
@@ -58,60 +60,13 @@ class GChart_ extends Application {
         ps.show
         // root.widthProperty().addListener(ne)
         chart.draw(WeightChart, false, SD)
+        /*
         val ds = DataStage.apply(ps)
         ds.show()
+        */
         // convertCSV()
     }
 
-    /*
-    def drawBase() = {
-        val inset = 30
-        val (xstart, xend) = (inset, can.getWidth - inset)
-        val (ystart, yend) = (inset, can.getHeight - inset)
-        val alpha = 0.5
-        // println(ystart, yend)
-        val gc = can.getGraphicsContext2D()
-        
-        // TODO. determine month gap by pixel gap 
-        val mrange = Range(3, 19).flatMap(y => Range(0, 6).map(y * 12 + _ * 2))
-        val monthMap = mapMaker(mrange.head, mrange.last, xstart, xend) _
-        gc.setGlobalAlpha(alpha)
-        gc.setStroke(Color.BLACK)
-        mrange.foreach({ m =>
-            val mx = monthMap(m)
-            if (m % 12 == 0) {
-                gc.setLineWidth(2)
-                gc.setGlobalAlpha(1.0)
-                val mstr = (m / 12).toString
-                val ms = textSize(mstr, gc.getFont())
-                gc.strokeText(mstr, mx - ms._1 / 2, yend + (ms._2 + 5))
-                gc.setGlobalAlpha(alpha)
-            } else {
-                gc.setLineWidth(1)
-            }
-            gc.strokeLine(mx, ystart, mx, yend)
-        })        
-
-        val msrange = (8, 20)
-        val measureMap = mapMaker(80, 200, yend, ystart) _
-        val measures = Range(8, 20).flatMap(u => Range(0, 6).map(u * 10 + _ * 2))
-        measures.foreach({ m =>
-            val my = measureMap(m)
-            println(m, my)
-            if (m % 10 == 0) {
-                gc.setLineWidth(2)
-                gc.setGlobalAlpha(1.0)
-                val mstr = (m / 10).toString
-                val ms = textSize(mstr, gc.getFont())
-                gc.strokeText(mstr, xstart - (ms._1 + 5), my + ms._2 / 2)
-                gc.setGlobalAlpha(alpha)
-            } else {
-                gc.setLineWidth(1)
-            }
-            gc.strokeLine(xstart, my, xend, my)
-        })
-    }
-    */
     def convertCSV() = {
         def helper(l: String) = {
             val ws = l.split(",").toList
